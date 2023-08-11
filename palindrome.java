618407b74d460e7db437ee9fe4e427a13b6a8edd
Yogesh Aaryaveer <86904855+theaaryaveer@users.noreply.github.com>
2023-08-11 15:12:58 +0545

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,19 @@
+public class palindrome{
+    public static boolean isPalindrome(String str){
+        int n = str.length();
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                //not palindrome
+                return false;
+            }
+
+
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str = "racecar";
+        System.out.println(isPalindrome(str));
+    }
+    
+}
